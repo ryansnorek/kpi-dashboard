94349d93784fb1e305f501ddad4d44ebd53ee1fe
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-30 19:37:39 -0400

    ci

--- src/components/charts/BarChart.js
+++ src/components/charts/BarChart.js
@@ -3,7 +3,7 @@ import { Bar, Line } from "react-chartjs-2";
 function BarChart({ period }) {
   return (
     <div className="bar-chart">
-
+      {period === "day" && <Line height={"120%"} data={dayData} />}
       {period === "month" && <Bar height={"120%"} data={monthData} />}
       {period === "quarter" && <Bar height={"120%"} data={quarterData} />}
       {period === "year" && <Bar height={"120%"} data={yearData} />}
@@ -11,6 +11,19 @@ function BarChart({ period }) {
   );
 }
 export default BarChart;
+const dayData = {
+  labels: ["6", "12", "18", "24"],
+  datasets: [
+    {
+      label: "Today",
+      data: [65, 59, 80, 81],
+      fill: true,
+      borderColor: "rgb(233, 204, 74)",
+      backgroundColor: "rgb(233, 204, 74, .5)",
+      tension: 0.1,
+    },
+  ],
+};
 const monthData = {
   labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
   datasets: [
@@ -18,19 +31,19 @@ const monthData = {
       label: "Estimated",
       data: [350, 290, 100, 710],
       backgroundColor: "rgb(218, 218, 218)",
-      tension: 0.1
+      tension: 0.1,
     },
     {
       label: "Guaranteed",
       data: [350, 400, 570, 660],
       backgroundColor: "rgb(15, 54, 113)",
-      tension: 0.1
+      tension: 0.1,
     },
     {
       label: "Predicted",
       data: [400, 350, 580, 660],
       backgroundColor: "rgb(233, 204, 74)",
-      tension: 0.1
+      tension: 0.1,
     },
   ],
 };
@@ -41,42 +54,55 @@ const quarterData = {
       label: "Estimated",
       data: [650, 590, 800, 810],
       backgroundColor: "rgb(218, 218, 218)",
-      tension: 0.1
+      tension: 0.1,
     },
     {
       label: "Guaranteed",
       data: [550, 500, 670, 760],
       backgroundColor: "rgb(15, 54, 113)",
-      tension: 0.1
+      tension: 0.1,
     },
     {
       label: "Predicted",
       data: [500, 450, 680, 760],
       backgroundColor: "rgb(233, 204, 74)",
-      tension: 0.1
+      tension: 0.1,
     },
   ],
 };
 const yearData = {
-  labels: ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
+  labels: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
   datasets: [
     {
       label: "Estimated",
       data: [883, 942, 712, 888, 1300, 1600, 1748, 2093, 1854, 1423, 1002, 932],
       backgroundColor: "rgb(218, 218, 218)",
-      tension: 0.1
+      tension: 0.1,
     },
     {
       label: "Guaranteed",
       data: [883, 942, 712, 888, 1300, 1600, 1748, 2093, 1854, 1423, 1002, 932],
       backgroundColor: "rgb(15, 54, 113)",
-      tension: 0.1
+      tension: 0.1,
     },
     {
       label: "Predicted",
       data: [883, 942, 712, 888, 1300, 1600, 1748, 2093, 1854, 1423, 1002, 932],
       backgroundColor: "rgb(233, 204, 74)",
-      tension: 0.1
+      tension: 0.1,
     },
   ],
 };
