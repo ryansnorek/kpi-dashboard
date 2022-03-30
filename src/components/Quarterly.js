e93b443c9bf018a62671011130bb8a581dd2da8c
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-30 18:19:04 -0400

    wip

--- src/components/Quarterly.js
+++ src/components/Quarterly.js
@@ -1,17 +1,19 @@
 import Meter from "./charts/Meter";
 import { getQuarter } from "../helper";
+import { useState } from "react";
 
 function Quarterly() {
   const currentQuarter = getQuarter();
-  const qtrID = (qtr) => `${currentQuarter === qtr && "highlight"}`;
-
+  const [quarter, setQuarter] = useState(currentQuarter);
+  const qtrID = (qtr) => `${quarter === qtr && "highlight"}`;
+  const handleClickQtr = (e) => setQuarter(e.target.name);
   return (
     <section className="quarterly">
       <div className="period">
-        <button id={qtrID("1")}>1Q22</button>
-        <button id={qtrID("2")}>2Q22</button>
-        <button id={qtrID("3")}>3Q22</button>
-        <button id={qtrID("4")}>4Q22</button>
+        <button id={qtrID("1")} name="1" onClick={handleClickQtr}>1Q22</button>
+        <button id={qtrID("2")} name="2" onClick={handleClickQtr}>2Q22</button>
+        <button id={qtrID("3")} name="3" onClick={handleClickQtr}>3Q22</button>
+        <button id={qtrID("4")} name="4" onClick={handleClickQtr}>4Q22</button>
       </div>
       <div className="meters">
         <Meter title="Estimated" meterValue={69} color={"rgb(218, 218, 218)"} />
--- src/helper/index.js
+++ src/helper/index.js
@@ -6,11 +6,11 @@ export function getQuarter() {
     const day = dateObj.getUTCDate();
     const year = dateObj.getUTCFullYear();
     
-    let quarter;
-    if (month < 4) quarter = "1";
-    else if (month < 7) quarter = "2";
-    else if (month < 10) quarter = "3";
-    else quarter = "4";
+    let qtr;
+    if (month < 4) qtr = "1";
+    else if (month < 7) qtr = "2";
+    else if (month < 10) qtr = "3";
+    else qtr = "4";
 
-    return quarter;
+    return qtr;
 }
