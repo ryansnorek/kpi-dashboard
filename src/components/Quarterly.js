0076bee64beef40d2f1965f1b3639d854f164d59
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-30 21:27:39 -0400

    rolling

--- src/components/Quarterly.js
+++ src/components/Quarterly.js
@@ -11,21 +11,21 @@ function Quarterly() {
     return currentQuarter - 1;
   });
   const qtrID = (qtr) => `${quarter === qtr && "highlight"}`;
-  const handleClickQtr = (e) => setQuarter(e.target.name);
+  const handleClickQtr = (e) => setQuarter(Number(e.target.name));
 
   return (
     <section className="quarterly">
       <div className="period">
-        <button id={qtrID(1)} name="1" onClick={handleClickQtr}>
+        <button id={qtrID(1)} name={1} onClick={handleClickQtr}>
           {labels[0]}
         </button>
-        <button id={qtrID(2)} name="2" onClick={handleClickQtr}>
+        <button id={qtrID(2)} name={2} onClick={handleClickQtr}>
           {labels[1]}
         </button>
-        <button id={qtrID(3)} name="3" onClick={handleClickQtr}>
+        <button id={qtrID(3)} name={3} onClick={handleClickQtr}>
           {labels[2]}
         </button>
-        <button id={qtrID(4)} name="4" onClick={handleClickQtr}>
+        <button id={qtrID(4)} name={4} onClick={handleClickQtr}>
           {labels[3]}
         </button>
       </div>
